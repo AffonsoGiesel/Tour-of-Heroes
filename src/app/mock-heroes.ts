1fe4f829e50d6e012a1ee0f28461eaedb3d09342
Affonso Giesel <affonsogiesel19@hotmail.com>
2018-05-09 18:55:43 -0300

    M

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Batman' },
+  { id: 12, name: 'Batman 2' },
+  { id: 13, name: 'Batman 3' },
+  { id: 14, name: 'Batman 4' },
+  { id: 15, name: 'Batman 5' },
+  { id: 16, name: 'Batman 6' },
+  { id: 17, name: 'Batman 7' },
+  { id: 18, name: 'Batman 8' },
+  { id: 19, name: 'Batman 9' },
+  { id: 20, name: 'Batman 10' }
+];
